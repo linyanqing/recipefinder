5c84a8b9cb52dfef9699088aeae8deb6aa28013f
linyanqing <linyanqing@gmail.com>
2014-02-21 18:07:03 +1100

    update unit test for no receipts in RecipeFinderServiceImpl

--- src/test/java/info/yanqing/recipefinder/service/RecipeFinderServiceImplTest.java
+++ src/test/java/info/yanqing/recipefinder/service/RecipeFinderServiceImplTest.java
@@ -27,114 +27,114 @@ import java.util.List;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext-test.xml")
-public class RecipeFinderServiceImplTest {
-
-
-    private final SimpleDateFormat DDMMYYYY = new SimpleDateFormat("dd/MM/yyyy");
-
-    @Autowired
-    private RecipeFinderService recipeFinderService;
-
-
-    @Test
-    public void testGetFridgeItemsFromString() throws Exception {
-        //construct content
-        String content = "bread,10,slices,25/12/2014\n" +
-                "cheese,10,slices,25/12/2014\n" +
-                "butter,250,grams,25/12/2014\n" +
-                "peanut butter,250,grams,2/12/2014\n" +
-                "mixed salad,150,grams,26/12/2014";
-
-        //execute method
-        List<FridgeItem> fridgeItemList =  recipeFinderService.getFridgeItemsFromString(content);
-
-        //assertion for list size
-        Assert.assertEquals(5, fridgeItemList.size());
-
-        //assertion for list element
-        Date useBy = DDMMYYYY.parse("26/12/2014");
-        FridgeItem expectedFridgeItem = new FridgeItem("mixed salad", 150, MeasureUnit.grams, useBy);
-        Assert.assertEquals(expectedFridgeItem, fridgeItemList.get(4));
-
-    }
-
-    @Test
-    public void testRecipeItemsFromString() throws IOException {
-        String content = "[    {        \"name\":\"grilled cheese on toast\",        \"ingredients\":[            {                \"item\":\"bread\",                \"amount\":\"2\",                \"unit\":\"slices\"            },            {                \"item\":\"cheese\",                \"amount\":\"2\",                \"unit\":\"slices\"            }        ]    }    ,    {        \"name\":\"salad sandwich\",        \"ingredients\":[            {                \"item\":\"bread\",                \"amount\":\"2\",                \"unit\":\"slices\"            },            {                \"item\":\"mixed salad\",                \"amount\":\"100\",                \"unit\":\"grams\"            }        ]    }]";
-    
-        //execute method
-        List<Recipe> recipeList = recipeFinderService.getRecipesFromString(content);
-
-         //assertion for list size
-        Assert.assertEquals(2, recipeList.size());
-        //assertion for list element
-    }
-
-    @Test
-    public void testGetValidRecipe() throws Exception {
-        //getValidRecipe(List<Recipe> recipes, List<FridgeItem> ingredients)
-
-        String content = "[    {        \"name\":\"grilled cheese on toast\",        \"ingredients\":[            {                \"item\":\"bread\",                \"amount\":\"2\",                \"unit\":\"slices\"            },            {                \"item\":\"cheese\",                \"amount\":\"2\",                \"unit\":\"slices\"            }        ]    }    ,    {        \"name\":\"salad sandwich\",        \"ingredients\":[            {                \"item\":\"bread\",                \"amount\":\"2\",                \"unit\":\"slices\"            },            {                \"item\":\"mixed salad\",                \"amount\":\"100\",                \"unit\":\"grams\"            }        ]    }]";
-
-        //execute method
-        List<Recipe> recipes = recipeFinderService.getRecipesFromString(content);
-
-        content = "bread,10,slices,22/12/2014\n" +
-                        "cheese,10,slices,23/12/2014\n" +
-                        "butter,250,grams,24/12/2014\n" +
-                        "peanut butter,250,grams,2/12/2014\n" +
-                        "mixed salad,150,grams,26/12/2014";
-
-        //execute method
-        List<FridgeItem> fridgeItemList =  recipeFinderService.getFridgeItemsFromString(content);
-
-        String recipe = recipeFinderService.getValidRecipe(recipes, fridgeItemList);
-
-        Assert.assertEquals("grilled cheese on toast", recipe);
-
-
-        content = "bread,10,slices,22/12/2014\n" +
-                                "cheese,10,slices,23/12/2014\n" +
-                                "butter,250,grams,24/12/2014\n" +
-                                "peanut butter,250,grams,2/12/2014\n" +
-                                "mixed salad,150,grams,22/12/2014";
-
-                //execute method
-                fridgeItemList =  recipeFinderService.getFridgeItemsFromString(content);
-        recipe = recipeFinderService.getValidRecipe(recipes, fridgeItemList);
-
-               Assert.assertEquals("salad sandwich", recipe);
-
-    }
-
-    @Test
-    public void testGetValidFridgeItems() throws ParseException {
-
-        //construct fridge items
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DATE, 1);
-        Date useBy = new Date(c.getTimeInMillis());
-
-        c.add(Calendar.DATE, -5);
-        Date useByExpired = new Date(c.getTimeInMillis());
-
-        FridgeItem expiredFridgeItem = new FridgeItem("mixed salad", 150, MeasureUnit.grams, useByExpired);
-        FridgeItem fridgeItem = new FridgeItem("bread", 10, MeasureUnit.grams, useBy);
-
-        List<FridgeItem> fridgeItemList = new ArrayList<FridgeItem>();
-        fridgeItemList.add(expiredFridgeItem);
-
-        //assertion for expired fridge items
-        List<FridgeItem> validFridgeItemList = recipeFinderService.getValidFridgeItems(fridgeItemList);
-        Assert.assertEquals(0, validFridgeItemList.size());
-
-        //assertion for valid fridge items
-        fridgeItemList.add(fridgeItem);
-        validFridgeItemList = recipeFinderService.getValidFridgeItems(fridgeItemList);
-        Assert.assertEquals(1, validFridgeItemList.size());
-
-
-
-    }
-
+public class RecipeFinderServiceImplTest
+{
+	
+	private final SimpleDateFormat DDMMYYYY = new SimpleDateFormat("dd/MM/yyyy");
+	
+	@Autowired
+	private RecipeFinderService recipeFinderService;
+	
+	@Test
+	public void testGetFridgeItemsFromString() throws Exception
+	{
+		// construct content
+		String content =
+				"bread,10,slices,25/12/2014\n" + "cheese,10,slices,25/12/2014\n" + "butter,250,grams,25/12/2014\n"
+						+ "peanut butter,250,grams,2/12/2014\n" + "mixed salad,150,grams,26/12/2014";
+		
+		// execute method
+		List<FridgeItem> fridgeItemList = recipeFinderService.getFridgeItemsFromString(content);
+		
+		// assertion for list size
+		Assert.assertEquals(5, fridgeItemList.size());
+		
+		// assertion for list element
+		Date useBy = DDMMYYYY.parse("26/12/2014");
+		FridgeItem expectedFridgeItem = new FridgeItem("mixed salad", 150, MeasureUnit.grams, useBy);
+		Assert.assertEquals(expectedFridgeItem, fridgeItemList.get(4));
+		
+	}
+	
+	@Test
+	public void testRecipeItemsFromString() throws IOException
+	{
+		String content =
+				"[    {        \"name\":\"grilled cheese on toast\",        \"ingredients\":[            {                \"item\":\"bread\",                \"amount\":\"2\",                \"unit\":\"slices\"            },            {                \"item\":\"cheese\",                \"amount\":\"2\",                \"unit\":\"slices\"            }        ]    }    ,    {        \"name\":\"salad sandwich\",        \"ingredients\":[            {                \"item\":\"bread\",                \"amount\":\"2\",                \"unit\":\"slices\"            },            {                \"item\":\"mixed salad\",                \"amount\":\"100\",                \"unit\":\"grams\"            }        ]    }]";
+		
+		// execute method
+		List<Recipe> recipeList = recipeFinderService.getRecipesFromString(content);
+		
+		// assertion for list size
+		Assert.assertEquals(2, recipeList.size());
+		// assertion for list element
+	}
+	
+	@Test
+	public void testGetValidRecipe() throws Exception
+	{
+		// getValidRecipe(List<Recipe> recipes, List<FridgeItem> ingredients)
+		
+		String content =
+				"[    {        \"name\":\"grilled cheese on toast\",        \"ingredients\":[            {                \"item\":\"bread\",                \"amount\":\"2\",                \"unit\":\"slices\"            },            {                \"item\":\"cheese\",                \"amount\":\"2\",                \"unit\":\"slices\"            }        ]    }    ,    {        \"name\":\"salad sandwich\",        \"ingredients\":[            {                \"item\":\"bread\",                \"amount\":\"2\",                \"unit\":\"slices\"            },            {                \"item\":\"mixed salad\",                \"amount\":\"100\",                \"unit\":\"grams\"            }        ]    }]";
+		
+		// execute method
+		List<Recipe> recipes = recipeFinderService.getRecipesFromString(content);
+		
+		content =
+				"bread,10,slices,22/12/2014\n" + "cheese,10,slices,23/12/2014\n" + "butter,250,grams,24/12/2014\n"
+						+ "peanut butter,250,grams,2/12/2014\n" + "mixed salad,150,grams,26/12/2014";
+		
+		// execute method
+		List<FridgeItem> fridgeItemList = recipeFinderService.getFridgeItemsFromString(content);
+		
+		String recipe = recipeFinderService.getValidRecipe(recipes, fridgeItemList);
+		
+		Assert.assertEquals("grilled cheese on toast", recipe);
+		
+		content =
+				"bread,10,slices,22/12/2014\n" + "cheese,10,slices,23/12/2014\n" + "butter,250,grams,24/12/2014\n"
+						+ "peanut butter,250,grams,2/12/2014\n" + "mixed salad,150,grams,22/12/2014";
+		
+		// execute method
+		fridgeItemList = recipeFinderService.getFridgeItemsFromString(content);
+		recipe = recipeFinderService.getValidRecipe(recipes, fridgeItemList);
+		
+		Assert.assertEquals("salad sandwich", recipe);
+		
+		recipe = recipeFinderService.getValidRecipe(recipes, new ArrayList<FridgeItem>());
+		
+		Assert.assertEquals("Order Takeout", recipe);
+		
+	}
+	
+	@Test
+	public void testGetValidFridgeItems() throws ParseException
+	{
+		
+		// construct fridge items
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DATE, 1);
+		Date useBy = new Date(c.getTimeInMillis());
+		
+		c.add(Calendar.DATE, -5);
+		Date useByExpired = new Date(c.getTimeInMillis());
+		
+		FridgeItem expiredFridgeItem = new FridgeItem("mixed salad", 150, MeasureUnit.grams, useByExpired);
+		FridgeItem fridgeItem = new FridgeItem("bread", 10, MeasureUnit.grams, useBy);
+		
+		List<FridgeItem> fridgeItemList = new ArrayList<FridgeItem>();
+		fridgeItemList.add(expiredFridgeItem);
+		
+		// assertion for expired fridge items
+		List<FridgeItem> validFridgeItemList = recipeFinderService.getValidFridgeItems(fridgeItemList);
+		Assert.assertEquals(0, validFridgeItemList.size());
+		
+		// assertion for valid fridge items
+		fridgeItemList.add(fridgeItem);
+		validFridgeItemList = recipeFinderService.getValidFridgeItems(fridgeItemList);
+		Assert.assertEquals(1, validFridgeItemList.size());
+		
+	}
+	
 }
